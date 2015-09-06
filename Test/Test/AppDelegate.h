e4783b0f02dd5ada89dad564241d861aa15537fa
雨天记忆 <956167738@qq.com>
2015-09-06 20:01:06 +0800

    创建的空工程

---
+++ Test/Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by 雨天记忆 on 15/9/6.
+//  Copyright (c) 2015年 Rebirth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test/Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by 雨天记忆 on 15/9/6.
+//  Copyright (c) 2015年 Rebirth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
